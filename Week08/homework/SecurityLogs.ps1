43f3658ca7a6117c320b27d4dd2e2cbe26a6cd55
unknown <nilesh.khetrapal@mymail.champlain.edu>
2021-03-21 08:02:09 +0530

    Did it

---
+++ Week08/homework/SecurityLogs.ps1
@@ -0,0 +1,15 @@
+﻿#Storyline: Review the security Event log
+
+#User input
+
+#Directory to save files:
+$myDir = "C:\Users\ASUS TUF GAMING\Desktop\"
+
+#List all the available windows event logs
+Get-EventLog -List
+
+#Create a prompt to allow user to select the log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+$searchPhrase = Read-host -Prompt "Please enter the value you want to search for"
+#Print the results for the log
+Get-EventLog -LogName $readLog | where {$_.Message -ilike "$searchPhrase" } | Export-Csv -NoTypeInformation -Path "$myDir\securityLogs.csv"
---
+++ Week08/homework/email.ps1
@@ -0,0 +1,17 @@
+﻿#Storyline: send an email
+#Variable can have ana underscore or any alphanumeric value
+
+#body of the email
+$msg = "General Kenobi"
+
+#Echo to the screen
+write-host -BackgroundColor Red -ForegroundColor White $msg
+
+#Email from Address
+$email = "nilesh.khetrapal@mymail.champlain.edu"
+
+#To address
+$toEmail = "deployer@csi-web"
+
+#Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "Hello There" -body $msg -SmtpServer 192.168.6.71
