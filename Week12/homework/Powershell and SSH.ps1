4e565fe4e392bf50b432f178167efdf962d01165
nileshkhetrapal <71113185+nileshkhetrapal@users.noreply.github.com>
2021-04-18 08:36:03 +0530

    Create Powershell and SSH.ps1
    done

---
+++ Week12/homework/Powershell and SSH.ps1
@@ -0,0 +1,19 @@
+cls
+# Login to a remote SSH server
+#New-SSHSession -ComputerName '192.168.0.28' —Credential (Get-credential kali)
+<#
+while ($true) {
+# Add a prompt to run commands
+$the_cmd = read-host -Prompt "please enter a command"
+# Run command on remote SSH server
+(Invoke-SSHCommand -index 0 $the_cmd).Output
+}
+#>
+
+#I am not on campus, so I used a Kali VM as my SSH server
+Set-SCPFile -ComputerName '192.168.0.28' -Credential (Get-credential kali) `
+-RemotePath '/home/kali' -LocalFile 'C:\Users\nilek\OneDrive\Documents\hello.zip' -Verbose
+#made it verbose to see if the file was uploaded properly or not.
+
+#Get-SCPFile -ComputerName '192.168.0.28' -Credential (Get-credential kali) `
+#-RemotePath '/home/kali' -LocalFile 'C:\Users\nilek\OneDrive\Documents\hello.zip'
